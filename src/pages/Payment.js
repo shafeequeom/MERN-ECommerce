aab508551feb6fdf259fdcb7a1b0d9f3d2be18e4
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2022-02-19 14:31:42 +0400

    Stripe Payment Intergation

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,7 +1,20 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { StripePayment } from "../components/payment/StripePayment";
+import "../stripe.css";
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+
 const Payment = () => {
   return (
-    <div className="container p-4">
-      <div>Complete payment</div>
+    <div className="container p-4 text-center">
+      <div className="text-center h4">Complete payment</div>
+      <Elements stripe={promise}>
+        <div className="col-md-8 offset-md-2">
+          {/* <p>Complete payment</p> */}
+          <StripePayment></StripePayment>
+        </div>
+      </Elements>
     </div>
   );
 };
