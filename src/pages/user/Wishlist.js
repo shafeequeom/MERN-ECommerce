a0b721bfc6266398fa5c74f35d6f94ccecccbdaa
Shafeeque O M <shafeequeom7@gmail.com>
2022-02-20 19:47:24 +0400

    Wishlist fixes

--- src/pages/user/Wishlist.js
+++ src/pages/user/Wishlist.js
@@ -3,11 +3,16 @@ import { useSelector } from "react-redux";
 import UserNav from "../../components/nav/UserNav";
 import { getWishList, removeFromWishlist } from "../../functions/user";
 import { Link } from "react-router-dom";
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import _ from "lodash";
+import { toast } from "react-toastify";
+import { Tooltip } from "antd";
 
 const Wishlist = () => {
   const [wishlist, setWishlist] = useState([]);
   const { user } = useSelector((state) => ({ ...state }));
+  let dispatch = useDispatch();
 
   useEffect(() => {
     loadWishList();
@@ -20,7 +25,38 @@ const Wishlist = () => {
   };
 
   const handleRemoveWishList = (productId) => {
-    getWishList(user.token, productId).then((res) => {
+    removeFromWishlist(user.token, productId).then((res) => {
+      loadWishList();
+    });
+  };
+
+  const handelMoveToCart = (product) => {
+    removeFromWishlist(user.token, product._id).then((res) => {
+      if (product.quantity == 0) {
+        toast.error(`${product.title} is out of stock`);
+        return;
+      }
+      let cart = [];
+      if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+          cart = JSON.parse(localStorage.getItem("cart"));
+        }
+        cart.push({
+          ...product,
+          count: 1,
+        });
+
+        let unique = _.uniqWith(cart, _.isEqual);
+        localStorage.setItem("cart", JSON.stringify(unique));
+        dispatch({
+          type: "ADD_TO_CART",
+          payload: unique,
+        });
+        dispatch({
+          type: "SET_DRAWER",
+          payload: true,
+        });
+      }
       loadWishList();
     });
   };
@@ -32,13 +68,22 @@ const Wishlist = () => {
           <UserNav></UserNav>
         </div>
         <div className="col">
+          <h4>Wishlist</h4>
           {wishlist.map((w) => (
             <div key={w._id} className="alert alert-secondary">
               <Link to={`product/${w.slug}`}>{w.title}</Link>
+              <Tooltip title="Remove from wishlist"></Tooltip>
               <DeleteOutlined
-                className="text-danger"
+                className="text-danger float-right"
                 onClick={() => handleRemoveWishList(w._id)}
               />
+              <Tooltip title="Move to cart"></Tooltip>
+              <Tooltip title="Move to cart">
+                <ShoppingCartOutlined
+                  className="text-danger mr-4 float-right"
+                  onClick={() => handelMoveToCart(w)}
+                />
+              </Tooltip>
             </div>
           ))}
         </div>
